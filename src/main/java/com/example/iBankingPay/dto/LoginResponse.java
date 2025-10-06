ed1a209764f2cdb665438b0fc1d9fe19c6e7a700
thothang <tn28112017@gmail.com>
2025-10-06 22:33:23 +0700

    payment service for ibanking midterm project

--- src/main/java/com/example/iBankingPay/dto/LoginResponse.java
+++ src/main/java/com/example/iBankingPay/dto/LoginResponse.java
@@ -1,30 +1,16 @@
 package com.example.iBankingPay.dto;
 
 import com.example.iBankingPay.entity.User;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class LoginResponse {
     private String token;
     private User user;
 
     public LoginResponse(String token, User user) {
         this.token = token;
-        this.user = user;
-    }
-
-    // Getters and setters
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
         this.user = user;
     }
 }
